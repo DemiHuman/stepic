a98e809ef847bbd1fc70d6cbc1d600c4d7f06649
Phantom <eropkinp@gmail.com>
2016-12-21 22:23:38 +0300

    0

---
+++ 1.12_6.py
@@ -0,0 +1,9 @@
+pr = int(input())
+
+if len(pr[1:]):
+    if pr == 5:
+        print(pr)
+    else:
+        print('qwe')
+else:
+    print('error')
---
+++ 2.2_3.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a <= b <= 10 and c <= d <= 10:
+    for i in range (a,b)
